2e649d4f5c26713aee8df214e95488eb5b37187d
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-03 01:16:53 -0300

    :pushpin: Estrutura do WidgetForm

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,11 @@
+export function WidgetForm() {
+  return (
+    <div
+      className="bg-zinc-900 p-4
+    relative rounded-2xl mb-4 flex flex-col justify-between
+    items-center shadow-lg w-[calc(100vw-2rem)] h-[280px] md:w-auto"
+    >
+      Olá mundo
+    </div>
+  );
+}
