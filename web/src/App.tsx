c7dc9676903b31daab226e8c3b5f3dec6753e658
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-02 23:31:56 -0300

    :pushpin: Separando em componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <h1 className="bg-rose-800 p-2">Hello world</h1>;
-}
+import { Widget } from "./components/Widget";
 
-export default App;
+export function App() {
+  return <Widget />;
+}
