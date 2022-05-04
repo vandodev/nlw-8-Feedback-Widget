5587a8fb11359ae1bc93a28a9b8b28f505c15d41
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-04 00:13:38 -0300

    :pushpin: Exibindo feedback conforme estado

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -45,20 +45,24 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button
-              key={key}
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              onClick={() => setfeedbackType(key as FeedbackType)}
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                onClick={() => setfeedbackType(key as FeedbackType)}
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Obrigado pelo seu feedback</p>
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}
