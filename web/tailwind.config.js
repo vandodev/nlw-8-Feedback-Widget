bcaee8cc2743fe8747f13185d9c007ca3fe4fdb6
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-04 02:33:42 -0300

    :pushpin: Formatando botão enviar

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: "#996DFF",
           500: "#8257e6",
+        },
+        borderRadius: {
+          md: "4px",
         },
       },
     },
