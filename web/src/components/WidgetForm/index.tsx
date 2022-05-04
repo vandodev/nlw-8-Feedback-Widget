d4b0e838805ecd5277b70e9f7a50ae56c6d4f0fc
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-04 00:38:33 -0300

    :pushpin: Separando componente FeedbackTypeStep

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from "../assets/bug.svg";
-import ideaImageUrl from "../assets/idea.svg";
-import thoughtImageUrl from "../assets/thought.svg";
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -30,7 +31,7 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setfeedbackType] = useState<FeedbackType | null>(null);
@@ -46,20 +47,7 @@ export function WidgetForm() {
       </header>
 
       {!feedbackType ? (
-        <div className="flex py-8 gap-2 w-full">
-          {Object.entries(feedbackTypes).map(([key, value]) => {
-            return (
-              <button
-                key={key}
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                onClick={() => setfeedbackType(key as FeedbackType)}
-              >
-                <img src={value.image.source} alt={value.image.alt} />
-                <span>{value.title}</span>
-              </button>
-            );
-          })}
-        </div>
+        <FeedbackTypeStep onFeedbackTypeChanged={setfeedbackType} />
       ) : (
         <p>Obrigado pelo seu feedback</p>
       )}
