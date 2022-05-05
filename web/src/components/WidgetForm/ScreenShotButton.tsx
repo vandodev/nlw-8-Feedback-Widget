72873c29d863dda4fd04a98abc901a4d250ca3a2
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-05 00:16:50 -0300

    :pushpin: Criando loading

--- web/src/components/WidgetForm/ScreenShotButton.tsx
+++ web/src/components/WidgetForm/ScreenShotButton.tsx
@@ -1,6 +1,7 @@
 import { Camera } from "phosphor-react";
 import html2canvas from "html2canvas";
 import { useState } from "react";
+import { Loading } from "../Loading";
 
 export function ScreenShotButton() {
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
@@ -18,7 +19,11 @@ export function ScreenShotButton() {
       onClick={handleTakeScreenshot}
       className="p-3 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-400 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     >
-      <Camera className="w-6 h-6" />
+      {isTakingScreenshot ? (
+        <Loading />
+      ) : (
+        <Camera className="w-6 h-6 text-zinc-100" />
+      )}
     </button>
   );
 }
