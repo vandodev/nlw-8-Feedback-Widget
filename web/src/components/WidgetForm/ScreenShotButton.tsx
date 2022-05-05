55da98b317827b4ad71f14ca52248d7977e10cfa
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-05 00:01:16 -0300

    :pushpin: Criando função handleTakeScreenshot

--- web/src/components/WidgetForm/ScreenShotButton.tsx
+++ web/src/components/WidgetForm/ScreenShotButton.tsx
@@ -1,8 +1,21 @@
 import { Camera } from "phosphor-react";
+import html2canvas from "html2canvas";
+import { useState } from "react";
+
 export function ScreenShotButton() {
+  const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
+
+  async function handleTakeScreenshot() {
+    setIsTakingScreenshot(true);
+    const canvas = await html2canvas(document.querySelector("html")!);
+    const base64Image = canvas.toDataURL("image/png");
+    setIsTakingScreenshot(false);
+  }
+
   return (
     <button
       type="button"
+      onClick={handleTakeScreenshot}
       className="p-3 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-400 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     >
       <Camera className="w-6 h-6" />
